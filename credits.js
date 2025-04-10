2e1b1292446186bf39195eca82fa559a2371a529
Gourami <softballpup12.ff@gmail.com>
2025-04-10 02:43:20 -0500

    Credits Page
    Created credits pop-up and added user-made frames

---
+++ credits.js
@@ -0,0 +1,9 @@
+function displayCredits() {
+    document.getElementById('fade').style.display='block';
+    document.getElementById('credits').style.display='block';
+}
+
+function hideCredits() {
+    document.getElementById('fade').style.display='none';
+    document.getElementById('credits').style.display='none';
+}
